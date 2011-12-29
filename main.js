5db245624e84e89fa15d2437eefc0307bb48aa37
nkchwfree <chwfree@gmail.com>
2011-12-29 18:59:19 +0800

    modify require queuer

--- main.js
+++ main.js
@@ -1,48 +1,40 @@
 var Parser = require('./lib/parser').Parser;
 
-var parser = new Parser(2);
+var parser = new Parser(5);
 
 
-//Hook
-var Hook = require('hook.io').Hook;
-var hook = new Hook( {
-    name: 'parser',   // 根据模块进行修改
-    'hook-host': '127.0.0.1',
-    debug: true
+var hook = require('devent').createDEvent('convertor');
+
+parser.on('task-finished', function(task){
+  hook.emit('task-finished', task);
 });
 
-hook.on('hook::ready', function(){
-  console.log('Hook Ready.');
-  parser.on('task-finished', function(task){
-    hook.emit('task-finished', task);
-  });
+parser.on('tidy-error', function(task){
+  hook.emit('task-error', task);
+});
 
-  parser.on('tidy-error', function(task){
-    hook.emit('task-error', task);
-  });
+parser.on('uri-error', function(task){
+  hook.emit('task-error', task);
+});
 
-  parser.on('uri-error', function(task){
-    hook.emit('task-error', task);
-  });
+parser.on('no-page-content', function(task){
+  hook.emit('task-error', task);
+  console.log('task-error');
+});
 
-  parser.on('no-page-content', function(task){
-    hook.emit('task-error', task);
-  });
-
-  hook.on('*::queued', function( task ){
-    if(task.queue=='page_content') {
-      console.log('emit event.');
-      parser.emit('has-task');
-    }
-  });
-
-  var func = function(){
+hook.on('queued', function( queue ){
+  console.log(queue);
+  if(queue=='page_content') {
+    console.log('emit event.');
     parser.emit('has-task');
-    setTimeout(func, 10000);
   }
-
-  func.call();
 });
 
+var func = function(){
+  parser.emit('has-task');
+  setTimeout(func, 10000);
+}
 
-hook.connect();
+func.call();
+
+
--- script/sub.js
+++ script/sub.js
@@ -1,23 +1,27 @@
-var argv = require('optimist').argv;
+var exec = require('child_process').exec;
 
 process.on('message', function(data) {
-  if(process.memoryUsage().rss > (150 * 1024 * 1024)) {
+  if(process.memoryUsage().rss > (60 * 1024 * 1024)) {
     process.exit();
   }
-  //console.log(__dirname+'/jquery-1.7.1.min.js');
-  var jsdom = require('jsdom');
-  //console.log(data);
 
-  jsdom.env(data.data.text, [__dirname+'/jquery-1.7.1.min.js'], function(errors, window) {
-    if( errors ){
-      process.send( { 'key': data.key, 'error': errors } );
-      //console.log("sub.js error "+errors);
-    } else {
-      window.__stopAllTimers();
-      var parser = require('../lib/matcher/'+data.data.site+'.'+data.data.type);
-      parser.parse( window, function( error, out ){
-        process.send( { 'key': data.key, 'data': out, 'error': error } );
-      } );
+  exec('/usr/local/php/bin/php '+__dirname+'/tidy.php', function(error, body, stderr){
+    if ( !error ) {
+      var jsdom = require('jsdom');
+      jsdom.env(body, [__dirname+'/jquery-1.7.1.min.js'], function(errors, window) {
+        if( errors ){
+          process.send( { 'key': data.key, 'error': errors } );
+        } else {
+          window.__stopAllTimers();
+          var parser = require('../lib/matcher/'+data.data.site+'.'+data.data.type);
+          parser.parse( window, function( error, match ){
+            process.send( { 'key': data.key, 'data': match, 'error': error } );
+          });
+        }
+      });
     }
-  });
+    else {
+      process.send( { 'key': data.key, 'error': 'tidy-error' } );
+    }
+  }).stdin.end( data.data.text, 'binary' );
 });
