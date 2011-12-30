437b4a5b51822c0d55b070089546d2886e82c4ce
nkchwfree <chwfree@gmail.com>
2011-12-30 17:17:05 +0800

    fix bug

--- script/sub.js
+++ script/sub.js
@@ -1,11 +1,12 @@
 var exec = require('child_process').exec;
+var config = require('../config/config').config;
 
 process.on('message', function(data) {
   if(process.memoryUsage().rss > (60 * 1024 * 1024)) {
     process.exit();
   }
 
-  exec('/usr/local/php/bin/php '+__dirname+'/tidy.php', function(error, body, stderr){
+  exec(config.php+' '+__dirname+'/tidy.php', function(error, body, stderr){
     if ( !error ) {
       var jsdom = require('jsdom');
       jsdom.env(body, [__dirname+'/jquery-1.7.1.min.js'], function(errors, window) {
