0e885d2dd06814305f974c5b70ea18f1c2ede807
nkchwfree <chwfree@gmail.com>
2012-04-26 20:46:19 +0800

    add bulletin

--- script/sub.js
+++ script/sub.js
@@ -2,13 +2,13 @@ var exec = require('child_process').exec;
 var config = require('../config/config').config;
 
 process.on('message', function(data) {
-    //×Ó½ø³ÌµÄÄÚ´æÏŞÖÆÄ¬ÈÏÊÇ64M¡£
+    //å­è¿›ç¨‹çš„å†…å­˜é™åˆ¶é»˜è®¤æ˜¯64Mã€‚
     var memory_limit = config[memory_limit]||64;
     if(process.memoryUsage().rss > ( memory_limit * 1024 * 1024)) {
         process.exit();
     }
 
-    //µ÷ÓÃtidyÔÊĞíÊ¹ÓÃµÄÄÚ´æÏŞÖÆ£¬Ä¬ÈÏÊÇ2M¡£
+    //è°ƒç”¨tidyå…è®¸ä½¿ç”¨çš„å†…å­˜é™åˆ¶ï¼Œé»˜è®¤æ˜¯2Mã€‚
     var max_buffer = config.max_buffer||2;
     exec(config.php+' '+__dirname+'/tidy.php', {maxBuffer:max_buffer*1024*1024}, function(error, body, stderr){
         if ( !error ) {
@@ -18,10 +18,16 @@ process.on('message', function(data) {
                     process.send( { 'key': data.key, 'error': errors } );
                 } else {
                     window.__stopAllTimers();
-                    var parser = require('../lib/matcher/'+data.data.site+'.'+data.data.type);
-                    parser.parse( window, data.data, function( error, match ){
-                        process.send( { 'key': data.key, 'data': match, 'error': error } );
-                    });
+
+                    try {
+                        var parser = require('../lib/matcher/'+data.data.site+'.'+data.data.type);
+                        parser.parse( window, data.data, function( error, match ){
+                            process.send( { 'key': data.key, 'data': match, 'error': error } );
+                        });
+                    }
+                    catch(e) {
+                        console.log(e);
+                    }
                 }
             });
         }
